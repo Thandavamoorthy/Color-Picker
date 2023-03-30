c2d1ca236c91a43bcc0656f10137b81604b62d85
Thandavamoorthy <thandavamoorthyk02@gmail.com>
2023-03-30 17:32:03 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const main = document.querySelector("main");
+const background = main.querySelector(".section");
+const colorName = main.querySelector(".color-name");
+const clickAnotherColor = main.querySelector(".click-btn");
+// const submit = main.querySelector(".submit-btn")
+const form = document.getElementById("form");
+
+const colors = ["silver", "gray", "white", "maroon", "red", "purple", "fuchsia", "green", "lime", "olive", "yellow", "navy", "blue", "teal", "aqua", "aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkgrey", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow", "grey", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyello", "lightgray", "lightgreen", "lightgrey", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "whitesmoke", "yellow", "yellowgreen"];
+
+clickAnotherColor.addEventListener('click', function () {
+    const randomNumber = getRandomNumber();
+    background.style.backgroundColor = colors[randomNumber];
+    colorName.textContent = colors[randomNumber];
+    console.log(randomNumber);
+    console.log(randomNumber);
+})
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    background.style.backgroundColor = document.getElementById("get-color").value.replaceAll(" ","").toLowerCase();
+    colorName.textContent = document.getElementById("get-color").value.replaceAll(" ","").toLowerCase();
+    document.getElementById("get-color").value = "";
+})
+
+function getRandomNumber() {
+    return (Math.floor(Math.random() * colors.length - 1))
+}
+
+
